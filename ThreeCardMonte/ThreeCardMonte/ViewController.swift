0205e6ffea1adb221329e60f484d4c14022a28e0
Tsering Lama <tsenyk@Tserings-MacBook-Pro.local>
2019-10-28 17:15:19 -0400

    got to flip the card with images

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+  @IBOutlet weak var displayLabel: UILabel!
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
+    @IBAction func pickingCard(_ sender: UIButton) {
+        var isEnabled: Bool
+         var randomNum = Int.random(in: 0...2)
+        if sender.tag == randomNum {
+           sender.setImage(UIImage(named: "kingCard"), for: .normal)
+            displayLabel.text = "you win"
+        } else {
+            sender.setImage(UIImage(named: "threeCard"), for: .normal)
+        }
+    }
 
+    
+    @IBAction func newGame(_ sender: UIButton) {
+        displayLabel.text = "Pick a card, any card"
+        
+    }
+    
 }
+
+    
+
 
