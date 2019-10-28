8fd40cb7d6cfc554ff873bb5eb26f050e15da30c
Tsering Lama <tsenyk@Tserings-MacBook-Pro.local>
2019-10-28 19:40:40 -0400

    finished the game. Still need to show the winner card once they pick the wrong one.

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -11,28 +11,50 @@ import UIKit
 class ViewController: UIViewController {
     
   @IBOutlet weak var displayLabel: UILabel!
+
     
-  override func viewDidLoad() {
+    @IBOutlet weak var card0: UIButton!
+    
+    @IBOutlet weak var card1: UIButton!
+    
+    @IBOutlet weak var card12: UIButton!
+    
+    override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
+    @IBAction func newGame(_ sender: UIButton) {
+           displayLabel.text = "Pick a card, any card"
+        
+        card0.setImage(UIImage(named: "cardBackRed"), for: .normal)
+        card1.setImage(UIImage(named: "cardBackRed"), for: .normal)
+        card12.setImage(UIImage(named: "cardBackRed"), for: .normal)
+        
+        card0.isEnabled = true
+        card1.isEnabled = true
+        card12.isEnabled = true
+           
+       }
 
+    
     @IBAction func pickingCard(_ sender: UIButton) {
-        var isEnabled: Bool
+        
          var randomNum = Int.random(in: 0...2)
         if sender.tag == randomNum {
            sender.setImage(UIImage(named: "kingCard"), for: .normal)
             displayLabel.text = "you win"
         } else {
             sender.setImage(UIImage(named: "threeCard"), for: .normal)
+            displayLabel.text = "you lose"
         }
+        card0.isEnabled = false
+        card1.isEnabled = false
+        card12.isEnabled = false
     }
+    
 
     
-    @IBAction func newGame(_ sender: UIButton) {
-        displayLabel.text = "Pick a card, any card"
-        
-    }
+   
     
 }
 
