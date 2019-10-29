401b2116f401f38ec87deda571000c2a511ceba1
Tsering Lama <tseringlama@pursuit.org>
2019-10-29 15:20:08 -0400

    officially finished the game

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -12,12 +12,11 @@ class ViewController: UIViewController {
     
   @IBOutlet weak var displayLabel: UILabel!
 
-    
     @IBOutlet weak var card0: UIButton!
     
     @IBOutlet weak var card1: UIButton!
     
-    @IBOutlet weak var card12: UIButton!
+    @IBOutlet weak var card2: UIButton!
     
     override func viewDidLoad() {
     super.viewDidLoad()
@@ -28,34 +27,40 @@ class ViewController: UIViewController {
         
         card0.setImage(UIImage(named: "cardBackRed"), for: .normal)
         card1.setImage(UIImage(named: "cardBackRed"), for: .normal)
-        card12.setImage(UIImage(named: "cardBackRed"), for: .normal)
+        card2.setImage(UIImage(named: "cardBackRed"), for: .normal)
+       
         
         card0.isEnabled = true
         card1.isEnabled = true
-        card12.isEnabled = true
-           
+        card2.isEnabled = true
        }
 
     
     @IBAction func pickingCard(_ sender: UIButton) {
         
-         var randomNum = Int.random(in: 0...2)
+        var randomNum = Int.random(in: 0...2)
         if sender.tag == randomNum {
            sender.setImage(UIImage(named: "kingCard"), for: .normal)
             displayLabel.text = "you win"
         } else {
             sender.setImage(UIImage(named: "threeCard"), for: .normal)
             displayLabel.text = "you lose"
+            if card0.tag == randomNum {
+                sender.setImage(UIImage(named: "kingCard"), for: .normal)
+            }
+            if card1.tag == randomNum {
+                sender.setImage(UIImage(named: "kingCard"), for: .normal)
+            }
+            if card2.tag == randomNum {
+                sender.setImage(UIImage(named: "kingCard"), for: .normal)
+            }
         }
         card0.isEnabled = false
         card1.isEnabled = false
-        card12.isEnabled = false
+        card2.isEnabled = false
+        
     }
-    
 
-    
-   
-    
 }
 
     
